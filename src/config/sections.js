3f444629bd13420d25e3e3e9d49e83467f2557c6
Zimgo012 <rjccabelcina@gmail.com>
2026-06-22 18:39:14 -0400

    added configurations for md files  rendering

---
+++ src/config/sections.js
@@ -0,0 +1,22 @@
+export const sections = {
+    projects: {
+        label: "Projects",
+        icon: "/w2k-programs.ico",
+        collection: "projects",
+        template: "projectPreview"
+    },
+
+    blogs: {
+        label: "Blogs",
+        icon: "/w95_64.ico",
+        collection: "blogs",
+        template: "blogPreview"
+    },
+
+    others: {
+        label: "Others",
+        icon: "/w95_44.ico",
+        collection: "others",
+        template: "defaultPreview"
+    }
+};
---
+++ src/content.config.ts
@@ -0,0 +1,25 @@
+import {defineCollection} from "astro:content";
+import {glob} from "astro/loaders";
+import {z} from "zod";
+
+const projects = defineCollection({
+    loader: glob({base: 'src/content/md/projects', pattern: '*.md'}),
+    schema: z.object({
+        name: z.string(),
+        summary: z.string(),
+        projectlink: z.string(),
+        blogLink: z.string(),
+    })
+
+})
+
+const blogs = defineCollection({
+    loader: glob({base: 'src/content/md/blogs', pattern: '*.md'}),
+    schema: z.object({
+        title: z.string(),
+        summary: z.string(),
+        author: z.string(),
+
+    })
+})
+export const collections = {projects, blogs}
