717d734249bd0cc8080324af1fbd7c05d26ad79f
Zimgo012 <rjccabelcina@gmail.com>
2026-06-25 10:34:57 -0400

    added project and blog page

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,15 +3,20 @@ import {glob} from "astro/loaders";
 import {z} from "zod";
 
 const projects = defineCollection({
-    loader: glob({base: 'src/content/md/projects', pattern: '*.md'}),
-    schema: z.object({
-        name: z.string(),
-        summary: z.string(),
-        projectlink: z.string(),
-        blogLink: z.string(),
-    })
+    loader: glob({base: "src/content/json/projects", pattern: "*.json"
+    }),
 
-})
+    schema: z.object({
+        projects: z.array(
+            z.object({
+                name: z.string(),
+                summary: z.string(),
+                projectlink: z.string(),
+                blogLink: z.string()
+            })
+        )
+    })
+});
 
 const blogs = defineCollection({
     loader: glob({base: 'src/content/md/blogs', pattern: '*.md'}),
